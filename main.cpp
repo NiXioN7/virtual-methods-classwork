9880f1f169f4e63d6344055c194f94b3b19c8704
NiXioN7 <137985111+NiXioN7@users.noreply.github.com>
2024-04-01 09:53:40 +0300

    Create main.cpp
    made an virtual methods in classes, so it optimized code a little bit, and added override to them just in case

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+class Movement
+{
+public:
+    virtual void movement()
+    {
+        
+    }
+};
+class Swim:public Movement
+{
+    void movement() override
+    {
+        cout << "Бульк - бульк" << endl;
+    }
+};
+class Teleport : public Movement
+{
+    void movement() override
+    {
+        int n = rand() % 100;
+        cout << "опа и я исчез, а куда не знаю" << endl;
+        if (n % 2 == 0)
+        {
+            cout << "Просчитался но где..." << endl;
+        }
+    }
+};
+class Fly: public Movement
+{
+    void movement() override
+    {
+        cout << "Бульк - бульк" << endl;
+    }
+};
+class Lay :public Movement
+{
+    void movement() override
+    {
+        cout << "Все, я спать" << endl;
+    }
+};
+class Person
+{
+public:
+    void move(Movement* move)
+    {
+        move->movement();
+    }
+};
+int main()
+{
+    setlocale(0, "ru");
+    srand(time(NULL));
+    Person person;
+    Swim swim;
+    person.move(&swim);
+    Fly fly;
+    person.move(&fly);
+    Teleport tp;
+    person.move(&tp);
+}
